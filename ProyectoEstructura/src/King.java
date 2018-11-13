87a2b1bec782cd806b8f945a05a238942c3014fa
Parra <A01229618@itesm.mx>
2018-11-13 17:45:14 -0600

    update

--- ProyectoEstructura/src/King.java
+++ ProyectoEstructura/src/King.java
@@ -15,5 +15,7 @@ public class King extends Pieza{
 	public void drawPieza(Graphics g){
 		if(this.side) g.drawImage(rey, 0, 0, null);//draw blanca
 		else g.drawImage(reyB, 0, 0, null); //draw negra
+		
+		//melapelas
 	}
 }
