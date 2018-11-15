c7f0cf3e1898eb039133902029d25df52de752b5
eregar <30888167+eregar@users.noreply.github.com>
2018-11-14 22:28:08 -0600

    Update Bishop.java

--- ProyectoEstructura/src/Bishop.java
+++ ProyectoEstructura/src/Bishop.java
@@ -12,67 +12,58 @@ public class Bishop extends Pieza{
 	}
 	
 	public boolean valida(Cuadro cuadro, int nextX,int nextY) {
-		int signo;
-		int sX;
-		int sY;
 		int cX=cuadro.getEx();
 		int cY=cuadro.getEy();
-		int dx=cX-nextX;
-		int dy=cY-nextY;
+		int dx=Math.abs(cX-nextX);
+		int dy=Math.abs(cY-nextY);
 		
-		if((cuadro.getBoard().checkPiece(nextX, nextY) || cuadro.getBoard().getCuadros()[nextY][nextX].getPieza()==null)) {
-			System.out.println(cX+","+cY);
-			if(dx==dy) {
-				if(dx>0) {
-					signo=-1;
-				}
-				else {
-					signo=1;
-				}
-				
-				cY=cY+signo;
-				cX=cX+signo;
-				System.out.println(cX+","+cY);
-				while(cX!=nextX) {
-					if(cuadro.getBoard().checkPiece(cX, cY)) {
-						return false;
+		if(dx==dy){
+			if(cX>nextX){
+				if(cY>nextY){
+					//arriba izquierda
+					cY--;
+					cX--;
+					while(cX!=nextX) {
+						if(cuadro.getBoard().checkPiece(cX, cY))return false;
+						cY--;
+						cX--;
 					}
-					cY=cY+signo;
-					cX=cX+signo;
-					System.out.println(cX+","+cY);
-				}
-				return true;
-			}
-			else if(dx==-dy) {
-				if(dy>0) {
-					sY=1;
-				}
-				else {
-					sY=-1;
-				}
-				if(dx>0) {
-					sX=-1;
-				}
-				else {
-					sX=1;
-				}
-				
-				cY+=sY;
-				cX+=sX;
-				System.out.println(cX+","+cY);
-				while(cX!=nextX) {
-					if(cuadro.getBoard().checkPiece(cX, cY)) {
-						return false;
+					return true;
+				}else{
+					//abajo izquierda
+					cY++;
+					cX--;
+					while(cX!=nextX) {
+						if(cuadro.getBoard().checkPiece(cX, cY))return false;
+						cY++;
+						cX--;
 					}
-					cY+=sY;
-					cX+=sX;
-					System.out.println(cX+","+cY);
+					return true;
 				}
-				return true;
+			}else{
+				if(cY>nextY){
+					//arriba derecha
+					cY--;
+					cX++;
+					while(cX!=nextX) {
+						if(cuadro.getBoard().checkPiece(cX, cY))return false;
+						cY--;
+						cX++;
+					}
+					return true;
+				}else{
+					//abajo derecha
+					cY++;
+					cX++;
+					while(cX!=nextX) {
+						if(cuadro.getBoard().checkPiece(cX, cY))return false;
+						cY++;
+						cX++;
+					}
+					return true;
+				}
 			}
-			else return false;
-		}
-		else return false;
+		}else return false;
 	}
 	public void drawPieza(Graphics g){
 		if(this.side) g.drawImage(rey, 0, 0,75,75, null);//draw blanca
